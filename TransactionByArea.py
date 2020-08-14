e49f7b5e0591ac771d83da015ca890525736a857
yandeqing <546218945@qq.com>
2020-08-14 13:43:36 +0800

    添加获取地区交易信息定时任务

--- TransactionByArea.py
+++ TransactionByArea.py
@@ -26,19 +26,19 @@ def getLast12Months():
     return date_list
 
 
-def getUrl(txtQueryDate):
+def getUrl(txtQueryDate,pageno=1):
     randomStr = float(random.randint(1, 100) / 1000000)
-    return f"http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1803_sczm&TABKEY=tab2&DATETIME={txtQueryDate}&random={randomStr}"
+    return f"http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1803_sczm&TABKEY=tab2&PAGENO={pageno}&DATETIME={txtQueryDate}&random={randomStr}"
 
 
-def start_main(strftime):
+def start_main(strftime,pageno=1):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate, sdch, br",
         "Accept-Language": "zh-CN,zh;q=0.8"
     }
-    data = requests.get(getUrl(strftime), headers=headers)
+    data = requests.get(getUrl(strftime,pageno), headers=headers)
     content = data.json()[1]['data']
     for item in content:
         del item['rowid']
@@ -46,7 +46,7 @@ def start_main(strftime):
             if key != 'dq':
                 item[key] = trim(item[key])
         item['type'] = 'transaction_by_area'
-        item['update_date'] = strftime
+        item['update_date'] = strftime+"-01"
         url = "http://139.129.229.205:8088"
         response = requests.post(url, json=item)
         print(f"insert {item}{response.text}")
@@ -63,7 +63,8 @@ def job_function():
     date_today = datetime.date.today()
     date = getLastMonth(date_today).strftime('%Y-%m')
     print(f"【main().date={date}】")
-    start_main(date)
+    start_main(date,1)
+    start_main(date,2)
     strftime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(f"{strftime} StockIndexesMainSz.py  end")
 
@@ -75,12 +76,12 @@ def getLastMonth(reference_date):
 
 
 if __name__ == '__main__':
-    # dates = getLast12Months()
-    # print(f"【().dates={dates}】")
-    # for item in dates:
-    #     start_main(item)
-    strftime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print(f"{strftime} TransactionByArea.py  start")
-    sched = BlockingScheduler()
-    sched.add_job(job_function, CronTrigger.from_crontab('17 9 1 * *'))
-    sched.start()
+    dates = getLast12Months()
+    print(f"【().dates={dates}】")
+    for item in dates:
+        start_main(item,2)
+    # strftime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    # print(f"{strftime} TransactionByArea.py  start")
+    # sched = BlockingScheduler()
+    # sched.add_job(job_function, CronTrigger.from_crontab('17 9 1 * *'))
+    # sched.start()
